39b27da639a93895540b399eed0a8d2dabbdaca8
software_house <sofwarehouse123@gmail.com>
2020-08-13 14:21:08 +0700

    initial commit

---
+++ Training Pegadaian batch 1/Feature/Auth/VM/ViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ViewModel.swift
+//  Training Pegadaian batch 1
+//
+//  Created by Mac on 13/8/20.
+//  Copyright © 2020 Pegadaian Digital. All rights reserved.
+//
+
+import Foundation
---
+++ Training Pegadaian batch 1/Feature/Auth/View/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Training Pegadaian batch 1
+//
+//  Created by Mac on 13/8/20.
+//  Copyright © 2020 Pegadaian Digital. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
